d37f3c862f73462e488146c6831de2da3e8c0ea8
Jair Reyes <reyesgonzalezmauricio84@gmail.com>
2026-05-24 14:00:49 -0600

    Corrección HTTPS detrás del proxy inverso de Render
    Render usa un reverse proxy que termina SSL antes de llegar al contenedor.
    Se configura trustProxies con wildcard para que Laravel detecte
    correctamente HTTPS y genere URLs seguras en formularios y redirects.

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -12,12 +12,17 @@ return Application::configure(basePath: dirname(__DIR__))
     )
     ->withMiddleware(function (Middleware $middleware) {
 
+    $middleware->trustProxies(at: '*', headers: \Illuminate\Http\Request::HEADER_X_FORWARDED_FOR |
+        \Illuminate\Http\Request::HEADER_X_FORWARDED_HOST |
+        \Illuminate\Http\Request::HEADER_X_FORWARDED_PORT |
+        \Illuminate\Http\Request::HEADER_X_FORWARDED_PROTO);
+
     $middleware->validateCsrfTokens(except: [
-        'registro', 
-        'usuarios/*', 
+        'registro',
+        'usuarios/*',
         'usuarios'
     ]);
-    
+
     $middleware->alias([
         'role' => \App\Http\Middleware\RoleMiddleware::class,
     ]);
